cfda2c23be0fde3b036ac401380a9d7f1c8d1edf
Zhixuan <zhixuanc@buffalo.edu>
2015-06-28 18:56:29 -0400

    add some debug code in the neighs.cc

--- src/parameters.h
+++ src/parameters.h
@@ -80,10 +80,10 @@ const double beta_P=2.;
 const double ata_P = 0.01;
 //----------------------------------------------------------------------------------------
 // for variable smooth length
-const int num_loop_P=5;
+const int num_loop_P=2;
 const double thresh_P=1e-5;
 const double C_smooth_P = 2.0;
-const double eta_smooth_P = 1.;
+const double eta_smooth_P = 1.2;
 
 //----------------------------------------------------------------------------------------
 //CFL coefficient for time step update
