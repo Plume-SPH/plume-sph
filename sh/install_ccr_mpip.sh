cebbf6fe68c15e3deed9639a1b8f2bdbbbd598c2
Zhixuan <zhixuanc@buffalo.edu>
2015-12-29 11:31:39 -0500

    add a sh script for configure the code for mpip and a slurm script for submit the job on ccr!

---
+++ sh/install_ccr_mpip.sh
@@ -0,0 +1,19 @@
+#load module
+module load hdf5/1.8.15p1
+module load mpip
+#clean up
+make clean
+make distclean
+
+#generate Makefile.in and config.h.in and other necessary files ---> need Makefile.am and configure.ac
+aclocal
+autoconf
+automake --add-missing
+
+
+#configure and generate Makefile --->options are flexible
+./configure --without-gdal --with-hdf5 --enable-debug --enable-parallel CC=mpicc CXX=mpicxx FC=mpifort CXXFLAGS="-L${MPIP_LIB} -L${UWND_LIB} -lmpiP -lm -lbfd -liberty -lunwind" 2>&1 | tee myconfig.out
+
+make 2>&1 | tee mymake.out
+make install 2>&1| tee mymakeinstall.out
+
